17434d7730155d81fbb38e1c4b195cb1157888bc
ayesha-nayyab <anayyab56@gmail.com>
2022-09-28 15:06:46 +0500

    commit

--- App.js
+++ App.js
@@ -1,17 +1,8 @@
 import React, { useEffect, useState, useRef } from "react";
-import MapView from "react-native-maps";
-import { StyleSheet, View, Dimensions, Text } from "react-native";
-import { Marker } from "react-native-maps";
-import * as Location from "expo-location";
-import FirebaseC from "./api/firebase";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { StyleSheet, View, Text } from "react-native";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
-/* import {
-  BannerAd,
-  BannerAdSize,
-  TestIds,
-} from "react-native-google-mobile-ads"; */
+
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
     shouldShowAlert: true,
@@ -20,9 +11,6 @@ Notifications.setNotificationHandler({
   }),
 });
 export default function App() {
-  const db = getFirestore(FirebaseC);
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
   const [expoPushToken, setExpoPushToken] = useState("");
   const [notification, setNotification] = useState(false);
   const notificationListener = useRef();
@@ -85,114 +73,23 @@ export default function App() {
       );
       Notifications.removeNotificationSubscription(responseListener.current);
     };
-    startLocation();
-    console.log("out of async");
-    if (location) {
-      watchLocation();
-    }
   }, []);
-
-  const startLocation = async () => {
-    let { status } = await Location.requestForegroundPermissionsAsync();
-    if (status !== "granted") {
-      setErrorMsg("Permission to access location was denied");
-      return;
-    }
-
-    let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
-    // console.log("location first time:" + location);
-    addLocation();
-  };
-
-  const addLocation = async () => {
-    try {
-      const docRef = await addDoc(collection(db, "Location"), {
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-      });
-      alert("Location saved");
-    } catch (e) {
-      alert(e.message);
-    }
-  };
-
-  const watchLocation = async () => {
-    let subscriber;
-    try {
-      subscriber = await Location.watchPositionAsync(
-        {
-          accuracy: Location.Accuracy.High,
-          timeInterval: 1000,
-          distanceInterval: 1,
-        },
-
-        (Location_update) => {
-          if (!Location_update) {
-            Location_update = location;
-          }
-          console.log("updated! ", Location_update.coords);
-          setLocation(Location_update);
-          addLocation();
-        }
-      );
-
-      console.log(location);
-    } catch (e) {
-      alert(e.message);
-    }
-    return subscriber;
-  };
 
   return (
     <View style={styles.container}>
-      {/* <MapView
-        style={styles.map}
-        showsUserLocation={true}
-        showsMyLocationButton={true}
-      >
-        {location ? (
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude,
-              longitude: location.coords.longitude,
-            }}
-            title={"My location"}
-          />
-        ) : null}
-      </MapView> */}
-      <View
-        style={{
-          flex: 2,
-          alignItems: "center",
-          justifyContent: "space-around",
-        }}
-      >
-        <Text>Your expo push token: {expoPushToken}</Text>
-        <View style={{ alignItems: "center", justifyContent: "center" }}>
-          <Text>
-            Title: {notification && notification.request.content.title}{" "}
-          </Text>
-          <Text>Body: {notification && notification.request.content.body}</Text>
-        </View>
+      <Text>Your expo push token: {expoPushToken}</Text>
+      <View style={{ alignItems: "center", justifyContent: "center" }}>
+        <Text>Title: {notification && notification.request.content.title}</Text>
+        <Text>Body: {notification && notification.request.content.body}</Text>
       </View>
-      {/* <BannerAd
-        unitId="ca-app-pub-3940256099942544/6300978111"
-        size={BannerAdSize.FULL_BANNER}
-      /> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    flex: 2,
     alignItems: "center",
-    justifyContent: "center",
-  },
-  map: {
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    justifyContent: "space-around",
   },
 });
